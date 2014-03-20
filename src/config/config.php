2d1f5d69ba5f46810461061376b105a158e3f238
hdioui <hdioui.abdeljalil@gmail.com>
2014-03-20 14:35:57 +0000

    updating config file

---
+++ src/config/config.php
@@ -0,0 +1,20 @@
+<?php 
+
+return array(
+	//you Facebook appID
+	'appId'  => 'appId',
+	//Your Facebook Secret
+	'secret' => 'secret',
+	//you can add scope according to your requirement
+	'scope' => 'email',
+	//url to handle when the user authorize your app
+	'redirect_uri' => 'http://yoururl.com/',
+	//the main page 
+	'page_id' => 'meditel',
+	//app title
+  'app_title' => 'Méditel avant Premiére',
+  //url to where your app is hosted
+  'app_url' => "https://envolgroupe.com/avant_premiere_last/",
+  //your app Description 
+  'app_desc' => "L’application Facebook« Avant première », permettra à Méditel de récompenser sa communauté de fan, en offrant des tickets de cinéma d’avant première de film, pour les cinémas des trois villes suivantes : Casablanca,Marrakech,et Fès."
+	);
